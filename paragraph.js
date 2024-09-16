5389fd0b396b2177d2012ab7d5b6873353d8332c
Darrel Soriano <dsoriano5@se14l0120-07.ad.bcit.ca>
2024-09-16 11:21:55 -0700

    Final Code

--- paragraph.js
+++ paragraph.js
@@ -4,6 +4,7 @@ let hobby = "draw";
 
 console.log(myName + " " + like + " to " + hobby + ", sleep and go biking. " + myName + " usually " + like + " to " + hobby + " cartoons, but sometimes " + myName + " " + hobby + " " + "realistic pictures of people or nature. " + myName + " often " + hobby + " on my free time, and " + myName + " plan to use it in a career in the future.");
 
+
 hobby = "sleep";
 
 console.log("Every night " + myName + " usually " + hobby + " for " + 7 + " hours.");
@@ -11,6 +12,7 @@ console.log("Every night " + myName + " usually " + hobby + " for " + 7 + " hour
 hobby = "go biking"; 
 
 console.log(myName + " don't often " + hobby + " anymore, but whenever " + myName + " do " + hobby + ", " + myName + " go for " + 4 + " hours or more.");
-
-
+// const and let variables are used good in the sentences and numbers are added correctly and where needed.
+// for the first console.log, the extra "+" does not create an error but I do not think is needed.
+// Reviewed by Richelle Tiongson
 
