77d4b049e117db40b85b45112b33be07880e5ee3
Darrel Soriano <dsoriano5@se14l0120-07.ad.bcit.ca>
2024-09-16 11:03:31 -0700

    final code

---
+++ paragraph.js
@@ -0,0 +1,16 @@
+const myName = "I";
+const like = "love";
+let hobby = "draw";
+
+console.log(myName + " " + like + " to " + hobby + ", sleep and go biking. " + myName + " usually " + like + " to " + hobby + " cartoons, but sometimes " + myName + " " + hobby + " " + "realistic pictures of people or nature. " + myName + " often " + hobby + " on my free time, and " + myName + " plan to use it in a career in the future.");
+
+hobby = "sleep";
+
+console.log("Every night " + myName + " usually " + hobby + " for " + 7 + " hours.");
+
+hobby = "go biking"; 
+
+console.log(myName + " don't often " + hobby + " anymore, but whenever " + myName + " do " + hobby + ", " + myName + " go for " + 4 + " hours or more.");
+
+
+
