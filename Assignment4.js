1af85e162a456495636d0454e7448ed8d2545bc6
Darrel Soriano <dsoriano5@se14l0120-07.ad.bcit.ca>
2024-09-23 11:37:46 -0700

    first commit

---
+++ Assignment4.js
@@ -0,0 +1,13 @@
+const A = null;
+let B = undefined;
+
+
+console.log(A + " " + "and" + " " + B + " are two people who both want to go into the Sanctum.");
+
+console.log("But they were both shy and they need a brave person to get in therefore:")
+
+B = 0;
+
+if(A !== B){
+    console.log("They could not enter.");
+}
